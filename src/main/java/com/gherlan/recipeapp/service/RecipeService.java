6b241423f329014b53f6df55212eefb1dd3644a6
robertgherlan@gmail.com <Robert.1994>
2020-10-26 23:04:56 +0200

    Added save method.

--- src/main/java/com/gherlan/recipeapp/service/RecipeService.java
+++ src/main/java/com/gherlan/recipeapp/service/RecipeService.java
@@ -1,9 +1,12 @@
 package com.gherlan.recipeapp.service;
 
+import com.gherlan.recipeapp.command.RecipeCommand;
 import com.gherlan.recipeapp.model.Recipe;
 
 import java.util.Optional;
 
 public interface RecipeService extends AbstractService<Recipe, Long> {
     Optional<Recipe> findByDescription(String description);
+
+    RecipeCommand save(RecipeCommand recipeCommand);
 }
--- src/main/java/com/gherlan/recipeapp/service/RecipeServiceImpl.java
+++ src/main/java/com/gherlan/recipeapp/service/RecipeServiceImpl.java
@@ -1,5 +1,8 @@
 package com.gherlan.recipeapp.service;
 
+import com.gherlan.recipeapp.command.RecipeCommand;
+import com.gherlan.recipeapp.converter.RecipeCommandToRecipe;
+import com.gherlan.recipeapp.converter.RecipeToRecipeCommand;
 import com.gherlan.recipeapp.model.Recipe;
 import com.gherlan.recipeapp.repository.RecipeRepository;
 import org.springframework.stereotype.Service;
@@ -8,16 +11,26 @@ import java.util.Optional;
 
 @Service
 public class RecipeServiceImpl extends AbstractServiceImpl<Recipe, Long> implements RecipeService {
-
     private final RecipeRepository recipeRepository;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeToRecipeCommand recipeToRecipeCommand, RecipeCommandToRecipe recipeCommandToRecipe) {
         super(recipeRepository);
         this.recipeRepository = recipeRepository;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
     }
 
     @Override
     public Optional<Recipe> findByDescription(String description) {
         return recipeRepository.findByDescription(description);
+    }
+
+    @Override
+    public RecipeCommand save(RecipeCommand recipeCommand) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(recipeCommand);
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
